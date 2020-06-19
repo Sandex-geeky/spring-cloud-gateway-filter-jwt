73ac4d76c65377c05c2418029e97f7b29dd6c33a
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-06-19 07:36:24 -0400

    Spring cloud gateway skeleton setup

---
+++ src/main/java/com/my/poc/springcloudgatewayfilterjwt/SpringCloudGatewayFilterJwtApplication.java
@@ -0,0 +1,13 @@
+package com.my.poc.springcloudgatewayfilterjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayFilterJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayFilterJwtApplication.class, args);
+	}
+
+}
